b793e602c8c0d6dfe9da746c07c46358f8f920da
jaemin <77824886+jaemin96@users.noreply.github.com>
2022-11-03 23:17:28 +0900

    [refactor] update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Counter } from 'components/Counter/counter';
-
-type Count = {
-    count: any;
-};
-
-interface State {
-    total: any;
-}
 
 function App() {
-    const [total, setTotal] = useState(0);
-
-    // const handleCount = (count: any) => {
-    //     console.log({ count });
-    //     setTotal((prev) => prev + count);
-    //     return count;
-    // };
-
-    const handleCount = () => {
-        setTotal((prev) => prev + 1);
-    };
-
-    React.useEffect(() => {
-        console.log({ total });
-    }, [total]);
-
-    return (
-        <>
-            <Counter handleCount={handleCount} />
-            <Counter handleCount={handleCount} />
-        </>
-    );
+    return <></>;
 }
 
 export default App;
