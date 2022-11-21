8764848ba191a1a693fcb9908f580ea3dd1a1084
jaemin <77824886+jaemin96@users.noreply.github.com>
2022-11-21 23:59:03 +0900

    [feat] es function diff

--- client/src/container/ES/EsPage.tsx
+++ client/src/container/ES/EsPage.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
 const EsPage = () => {
+    
+    function esFunction() {
+        this.text = "hello";
+
+        return {
+            text: "es5 & es6 function test",
+            es5: function(){return this.text;},
+            es6: ()=>{return this.text;} 
+        }
+    }
+    
     let list: any;
     (list = []).length = 7;
     list.fill(0);
 
-    return list.map((item: any, index: any) => (
-        <p key={index + 1}>{`${index} = ${index + 1}`}</p>
-    ));
+    const ESFunction = new esFunction();
+
+    console.log({ESFunction.es5()},{ESFunction.es6()})
+    
+    return <h1>end</h1>;
 };
 
 export default EsPage;
